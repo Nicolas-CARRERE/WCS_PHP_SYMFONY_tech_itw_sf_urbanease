812863bfc2a06cb36997ead68c5748c4d80cc24d
Nicolas Carrère <nicolas.carrere@example.com>
2026-03-25 07:36:13 +0100

    Fix migrations for PostgreSQL compatibility

--- src/Migrations/Version20181123132219.php
+++ src/Migrations/Version20181123132219.php
@@ -13,17 +13,13 @@ final class Version20181123132219 extends AbstractMigration
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
-
-        $this->addSql('CREATE TABLE boat (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, coord_x INT NOT NULL, coord_y INT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE tile (id INT AUTO_INCREMENT NOT NULL, type VARCHAR(255) NOT NULL, coord_x INT NOT NULL, coord_y INT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE boat (id SERIAL NOT NULL, name VARCHAR(255) NOT NULL, coord_x INT NOT NULL, coord_y INT NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE TABLE tile (id SERIAL NOT NULL, type VARCHAR(255) NOT NULL, coord_x INT NOT NULL, coord_y INT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
     }
 
     public function down(Schema $schema) : void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
-
         $this->addSql('DROP TABLE boat');
         $this->addSql('DROP TABLE tile');
     }
